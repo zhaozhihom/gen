2d496d12ee3641e52bc018d22d6451067fc5cc9b
zhaozhihong <zhaozhihong@bytedance.com>
2022-08-14 12:26:07 +0800

    add logs

--- generator.go
+++ generator.go
@@ -354,6 +354,9 @@ func (g *Generator) generateSingleQueryFile(data *genInfo) (err error) {
 	if structPkgPath == "" {
 		structPkgPath = g.modelPkgPath
 	}
+
+	_ = fmt.Sprintf("start render header...")
+
 	err = render(tmpl.Header, &buf, map[string]interface{}{
 		"Package":        g.queryPkgName,
 		"StructPkgPath":  structPkgPath,
@@ -363,15 +366,21 @@ func (g *Generator) generateSingleQueryFile(data *genInfo) (err error) {
 		return err
 	}
 
+	_ = fmt.Sprintf("success render header...")
+
 	structTmpl := tmpl.BaseStructWithContext
 	if g.judgeMode(WithoutContext) {
 		structTmpl = tmpl.BaseStruct
 	}
 
+	_ = fmt.Sprintf("start render struct...")
+
 	err = render(structTmpl, &buf, data.BaseStruct)
 	if err != nil {
 		return err
 	}
+
+	_ = fmt.Sprintf("success render struct...")
 
 	for _, method := range data.Interfaces {
 		err = render(tmpl.DIYMethod, &buf, method)
@@ -380,10 +389,14 @@ func (g *Generator) generateSingleQueryFile(data *genInfo) (err error) {
 		}
 	}
 
+	_ = fmt.Sprintf("start render method...")
+
 	err = render(tmpl.CRUDMethod, &buf, data.BaseStruct)
 	if err != nil {
 		return err
 	}
+
+	_ = fmt.Sprintf("success render method...")
 
 	defer g.successInfo(fmt.Sprintf("generate query file: %s/%s.gen.go", g.OutPath, data.FileName))
 	return g.output(fmt.Sprintf("%s/%s.gen.go", g.OutPath, data.FileName), buf.Bytes())
